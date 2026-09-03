fd6600c07ef43d36016e8dd2605d79597bd7501f
Stine <stine.fohrmann@proton.me>
2026-09-03 13:12:41 +0200

    verify formatting of time attributes

--- mdcheck.py
+++ mdcheck.py
@@ -6,11 +6,10 @@ INDENT = '     '
 REPORT_WIDTH = 60
 
 class MDChecker():
-    def __init__(self, input_file, minimal_attrs=None, global_attrs=None):
+    def __init__(self, input_file, minimal_attrs=None, given_attrs=None):
         self.input_file = input_file        # netCDF file to check
         self.minimal_attrs = minimal_attrs  # list of minimal required global attributes
-        self.global_attrs = global_attrs
-        self.missing_attrs = []
+        self.given_attrs = given_attrs
         self.errors = []
     
     def getMinimalRequirements(self):
@@ -26,9 +25,9 @@ class MDChecker():
             decode_times=False,
         )
         # get attributes
-        self.global_attrs = ds.attrs
+        self.given_attrs = ds.attrs
     
-    def printReport(self, missing_attrs):
+    def printReport(self):
         # Centered title and link to specifications
         print("-" * REPORT_WIDTH)
         print("\n" + 'MET Metadata Compliance Report'.center(REPORT_WIDTH))
@@ -39,17 +38,11 @@ class MDChecker():
         print(INDENT + f'File:   {self.input_file}')
 
         # Print results
-        if len(missing_attrs) <= 0:
+        if len(self.errors) <= 0:
             print(INDENT + 'All tests passed. All required attributes are defined.')
             print("-" * REPORT_WIDTH)
         else:
-            print(INDENT + f'Errors: {len(missing_attrs)}')
-            print('\n' + "-" * REPORT_WIDTH)
-
-            count = 1
-            for attr in missing_attrs:
-                print(INDENT + f'{count:2.0f}) Missing attribute: {attr['name']}')
-                count += 1
+            self.printErrors()
             print("-" * REPORT_WIDTH)
             
     def checkMinimalReqs(self):
@@ -60,20 +53,31 @@ class MDChecker():
         self.getMinimalRequirements()
         self.getGlobalAttrs()
 
+        # verify time attributes are given and formatted correctly
+        self.checkTimeAttrs()
+
+        # remove time attributes from list to be checked
+        # TODO get time attrs from json instead
+        required_time_attrs = ['time_coverage_start', 'time_coverage_end', 'date_created']
+
+        # verify other attributes are given
         for attr in self.minimal_attrs:
+            # skip time attrs
+            if attr['name'] in required_time_attrs:
+                continue
 
             # check whether required attributes exist  
-            if not attr['name'] in self.global_attrs:
-                self.missing_attrs.append(attr)
+            if not attr['name'] in self.given_attrs:
+                self.errors.append(Error(attr['name'], message='is not defined'))
         
         # Print results in terminal
-        self.printReport(self.missing_attrs)
+        self.printReport()
     
     def checkTimeFormat(self, attr_str):
         from utils import iso_to_dt64
 
         try:    # check if attribute defined
-            time_str = self.global_attrs[attr_str]
+            time_str = self.given_attrs[attr_str]
             try:    # check if valid ISO format
                 iso_to_dt64(time_str)
             except: # return error if invalid date or format
@@ -82,14 +86,24 @@ class MDChecker():
             return Error(attr=attr_str, message='is not defined')
     
     def checkTimeAttrs(self):
-        # time_coverage_start
-        # time_coverage_end
-        # date_created
+        ''' Checks whether the required time attributes are given and formatted in ISO 8601:2004 '''
+        
+        from utils import iso_to_dt64
 
+        # TODO get time attrs from json instead
         required_time_attrs = ['time_coverage_start', 'time_coverage_end', 'date_created']
 
         for attr_str in required_time_attrs:
-            result = self.checkTimeFormat(attr_str)
+            result = None
+            try:    # check if attribute defined
+                time_str = self.given_attrs[attr_str]
+                try:    # check if valid ISO format
+                    iso_to_dt64(time_str)
+                except: # return error if invalid date or format
+                    result = Error(attr=attr_str, message='is invalid date or date format')
+            except: # return error if attribute not defined
+                result = Error(attr=attr_str, message='is not defined')
+
             if isinstance(result, Error):
                 self.errors.append(result)
     
