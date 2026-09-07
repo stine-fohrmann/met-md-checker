797ba74e5c3f4f14c2ae5b8ce297397f7723437b
Stine <stine.fohrmann@proton.me>
2026-09-07 14:52:54 +0200

    use general func for checking attributes exist and are nonempty

--- mdcheck.py
+++ mdcheck.py
@@ -80,93 +80,74 @@ class MDChecker():
         
         # Print results in terminal
         self.printReport()
-    
-    # def checkTimeFormat(self, attr_str):
-    #     from utils import iso_to_dt64
 
-    #     try:    # check if attribute defined
-    #         time_str = self.given_attrs[attr_str]
-    #         try:    # check if valid ISO format
-    #             iso_to_dt64(time_str)
-    #         except: # return error if invalid date or format
-    #             return Error(attr=attr_str, message='is invalid date or date format')
-    #     except: # return error if attribute not defined
-    #         return Error(attr=attr_str, message='is not defined')
     
-    def checkTimeAttrs(self):
-        ''' Checks whether the required time attributes are given and formatted in ISO 8601:2004 '''
+    def _check(self, attr: str, validate=None) -> None:
+        try:
+            val = self.given_attrs[attr]
+            # verify not empty
+            if val.strip() == '':
+                self.errors.append(Error(attr=attr, message=f'is empty'))
+                return
+        except KeyError:
+            self.errors.append(Error(attr, message='is not defined'))
+            return
         
+        if validate:
+            validate(val)
+    
+    def _validate_time(self, attr: str, val):
         from utils import iso_to_dt64
 
-        # TODO get time attrs from json instead
+        # validate ISO format
+        try:
+            iso_to_dt64(val)
+        except ValueError:
+            self.errors.append(Error(attr=attr, message=f'"{val}" is an invalid date or date format'))
+
+    def checkTimeAttrs(self):
+        ''' Checks whether the required time attributes are given and formatted in ISO 8601:2004 '''
         required_time_attrs = ['time_coverage_start', 'time_coverage_end', 'date_created']
-
-        for attr_str in required_time_attrs:
-            result = None
-            try:    # check if attribute defined
-                time_str = self.given_attrs[attr_str]
-                try:    # check if valid ISO format
-                    iso_to_dt64(time_str)
-                except: # return error if invalid date or format
-                    self.errors.append(Error(attr=attr_str, message=f'"{time_str}" is an invalid date or date format'))
-            except: # return error if attribute not defined
-                self.errors.append(Error(attr=attr_str, message='is not defined'))
-
+        for attr in required_time_attrs:
+            self._check(attr, lambda v, a=attr: self._validate_time(a, v))
+    
+    def _validate_geo(self, attr: str, val):
+        v = float(val)
+        # verify at least 2 decimal points
+        if len(str(v).split('.')[1]) < 2:
+            self.errors.append(Error(attr, message=f'"{val}" has too few decimal places. Include at least 2 decimal places.'))
+        
+        # verify latitude between -90 and 90
+        if attr.startswith('geospatial_lat') and not -90 <= v <= 90:
+            self.errors.append(Error(attr, message=f'"{val}" is invalid. Must be between -90 and 90.'))
+        
+        # verify longitude between -180 and 180
+        if attr.startswith('geospatial_lon') and not -180 <= v <= 180:
+            self.errors.append(Error(attr, message=f'"{val}" is invalid. Must be between -180 and 180.'))
+    
     def checkGeospatial(self):
         '''Checks whether the geospatial bounds are given and are valid values'''
-
         required_geo_attrs = ['geospatial_lat_min', 'geospatial_lat_max', 'geospatial_lon_min', 'geospatial_lon_max']
-
-        for attr_str in required_geo_attrs:
-            result = None
-            try:    # check if attribute given
-                geo_str = self.given_attrs[attr_str]
-
-                # verify at least 2 decimal points
-                num_decimals = geo_str[::-1].find('.')
-                if num_decimals < 2:
-                    self.errors.append(Error(attr=attr_str, message=f'"{geo_str}" has too few decimal places. Include at least 2 decimal places.'))
-                
-                # verify latitude between -90 and 90
-                if (attr_str == 'geospatial_lat_min' or attr_str == 'geospatial_lat_max') and not (-90 <= float(geo_str) <= 90):
-                    print(float(geo_str))
-                    self.errors.append(Error(attr=attr_str, message=f'"{geo_str}" is invalid. Must be between -90 and 90.'))
-                
-                # verify longitude between -180 and 180
-                if (attr_str == 'geospatial_lon_min' or attr_str == 'geospatial_lon_max') and not (-180 <= float(geo_str) <= 180):
-                    self.errors.append(Error(attr=attr_str, message=f'"{geo_str}" is invalid. Must be between -180 and 180.'))
-                
-            except: # return error if attribute not defined
-                self.errors.append(Error(attr=attr_str, message='is not defined')   )
-
-    def checkPublisher(self):
-        '''Checks whether the publisher information is given correctly'''
+        for attr in required_geo_attrs:
+            self._check(attr, lambda v, a=attr: self._validate_geo(a, v))
+    
+    
+    def _validate_pub(self, attr: str, val):
         from utils import is_valid_url, is_valid_email
 
+        # validate url
+        if attr == 'publisher_url' and not is_valid_url(val):
+            self.errors.append(Error(attr=attr, message=f'"{val}" is not a valid URL'))
+
+        # validate email address
+        if attr == 'publisher_email' and not is_valid_email(val):
+            self.errors.append(Error(attr=attr, message=f'"{val}" is not a valid email address'))
+    
+    def checkPublisher(self):
+        '''Checks whether the publisher information is given correctly'''
         req_publisher_attrs = ['publisher_name', 'publisher_url', 'publisher_institution', 'publisher_email']
-
-        # check if given
-        for attr_str in req_publisher_attrs:
-            result = None
-            try:
-                pub_str = self.given_attrs[attr_str]
-
-                # verify not empty
-                if pub_str.strip() == '':
-                    self.errors.append(Error(attr=attr_str, message=f'is empty'))
-
-                match attr_str:
-                    case 'publisher_url':
-                        # validate url
-                        if not is_valid_url(pub_str):
-                            self.errors.append(Error(attr=attr_str, message=f'"{pub_str}" is not a valid URL'))
-                    case 'publisher_email':
-                        # validate email address
-                        if not is_valid_email(pub_str):
-                            self.errors.append(Error(attr=attr_str, message=f'"{pub_str}" is not a valid email address'))
-            except:
-                self.errors.append(Error(attr=attr_str, message='is not defined'))
-            
+        for attr in req_publisher_attrs:
+            self._check(attr, lambda v, a=attr: self._validate_pub(a, v))
 
     def printErrors(self):
         print(INDENT + f'Errors: {len(self.errors)}')
