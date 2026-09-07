06bb3464c30ccfe34f44307371e64077cbe0944a
Stine <stine.fohrmann@proton.me>
2026-09-07 11:46:38 +0200

    add helper funcs for validating url and email address

--- utils.py
+++ utils.py
@@ -22,3 +22,36 @@ def iso_to_dt64(iso_str):
     hms = iso_str.split('T')[1][:-1]
     dt64 = np.datetime64('T'.join([ymd, hms]))
     return dt64
+
+def is_valid_url(url: str):
+    from urllib.parse import urlparse
+    ''' Checks whether a URL is valid
+    
+    Parameters:
+    -----------
+    url : string
+        the URL to be validated
+    
+    Returns:
+    --------
+    boolean
+    '''
+    tokens = urlparse(url)
+    return all(getattr(tokens, attr) for attr in ('scheme', 'netloc'))
+
+def is_valid_email(email: str):
+    ''' Checks whether a URL is valid
+    
+    Parameters:
+    -----------
+    email : string
+        the email address to be validated
+    
+    Returns:
+    --------
+    boolean
+    '''
+    import re
+    reg = re.match("[^@]+@[^@]+\\.[^@]+", email)
+
+    return bool(reg)
